50534ecde178d52d6d587227e8c85949b7977a20
Iason Nissopoulos <IasonNissopoulos@users.noreply.github.com>
2018-04-15 16:40:50 +0300

    some changes

--- Unit Testing/src/main/java/unit_testing/MyFileUtilities.java
+++ Unit Testing/src/main/java/unit_testing/MyFileUtilities.java
@@ -5,15 +5,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class MyFileUtilities {
-
+	//Reads a file that contains grades and returns its content in an integer array
 	public int[] readFile(String filepath) throws FileNotFoundException {
 		//ArrayList<Integer> grades = new ArrayList<Integer>();
 		int[] grades = null;
 		int size = 0;
 		try {
+			//Scan the file in the filepath path
 			Scanner scanner = new Scanner(new File(filepath));
 
 			int i = 0;
+			//This is a way to get the file's length(size in lines) 
 			while(scanner.hasNextInt())
 			{
 			   //  grades[i++] = scanner.nextInt();
@@ -21,7 +23,7 @@ public class MyFileUtilities {
 			}
 			scanner = new Scanner(new File(filepath));
 			grades = new int[size];
-
+			//read each grade and append it to the grades arrray
 			while(scanner.hasNext()) {
 				grades[i++] = scanner.nextInt();
 			}
--- Unit Testing/src/test/java/unit_testing/MyArrayOperationsTest.java
+++ Unit Testing/src/test/java/unit_testing/MyArrayOperationsTest.java
@@ -14,13 +14,14 @@ public class MyArrayOperationsTest {
 		String filepath = ("resources//grades.txt");
 		MyFileUtilities mfu = mock(MyFileUtilities.class);
 		// Pre-define the results of the reverseNumber calls
-		when(mfu.readFile(filepath)).thenReturn(new int[]{0,2,9,4,1,6,7,7,10,3,8,1,1});
+		//when(mfu.readFile(filepath)).thenReturn(new int[]{0,2,9,4,1,6,7,7,10,3,8,1,1});
+		//when(mao.gradeFre(filepath)).thenReturn(new int[]{0,2,9,4,1,6,7,7,10,3,8,1,1});
 
 		
 		int[] testValues = {1,2,-3,-4};
 		// Call the reverseArray with the mocked MyMath instance
-		assertArrayEquals((new int[]{1,3,1,1,1,0,1,2,1,1,1}), 
-				mao.gradeFrequencies(filepath, mfu.readFile(filepath)));
+		int [] expected = new int[]{1,3,1,1,1,0,1,2,1,1,1};
+		assertArrayEquals(expected, mao.gradeFrequencies(filepath, mfu));
 	}
 
 }
