48ed5e1ad49dda08a1f0fde2bceabaff8a9bd0cc
Iason Nissopoulos <IasonNissopoulos@users.noreply.github.com>
2018-04-17 00:25:47 +0300

    Add MyMathTestSuite

---
+++ Unit Testing/src/test/java/unit_testing/MyMathTestSuite.java
@@ -0,0 +1,17 @@
+package unit_testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MyMathTest.class, MyMathTest_ReverseNumber_Parameterized.class})
+public class MyMathTestSuite {
+	/*
+	 * This class remains empty. Is used only as a holder 
+	 * of the above annotations.
+	 */
+
+
+}
