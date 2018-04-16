4fcf4bcdc3f8a4a4e38e5ab61d8b8b86c1445987
Iason Nissopoulos <IasonNissopoulos@users.noreply.github.com>
2018-04-17 00:40:08 +0300

    MyFileUtilitiesTest works correctly

--- Unit Testing/src/test/java/unit_testing/MyFileUtilitiesTest.java
+++ Unit Testing/src/test/java/unit_testing/MyFileUtilitiesTest.java
@@ -5,29 +5,20 @@ import static org.junit.Assert.*;
 import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 
-//import org.junit.Before;
-//import java.io.InputStream;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-//import org.junit.runner.RunWith;
-//import org.junit.runners.Parameterized;
-//import org.junit.runners.Parameterized.Parameter;
+
 
 
 public class MyFileUtilitiesTest {
 
-	/*@RunWith(Parameterized.class)
-	public class MyFileUtilitiesTest_FilePath_Parameterized {
-		
-		@Parameter(value = "src/test/resources/grades.txt")
-		public String filepath;
-	}*/
-	
-	
 	MyFileUtilities mfu = new MyFileUtilities();
+	//Create a Rule to catch the exceptions
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
+	//Test
 	@Test
 	public void test_readFile_RuleException() throws FileNotFoundException {
 		thrown.expect(InputMismatchException.class);
@@ -36,21 +27,17 @@ public class MyFileUtilitiesTest {
 	@Test
 	public void testEmptyFile() throws FileNotFoundException{
 		int[] methodOutput = mfu.readFile("src/test/resources/empty.txt");
-		int[] expected = null;
-		assertArrayEquals(expected, methodOutput);
+		int expected = 0;
+		assertEquals(expected, methodOutput.length);
 		
 	}
 	@Test
 	public void testReadFile() throws FileNotFoundException {
-		//ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		//InputStream file = classloader.getResourceAsStream("grades.txt");
-		//ClassLoader classLoader = getClass().getClassLoader();
-		//File file = new File(classLoader.getResource("somefile").getFile());
-		//String filepath = this.getClass().getResource("/grades.txt").toURI();
-		//System.out.println(file.getAbsolutePath());
+		//Call mfu to produce an output
 		int[] methodOutput = mfu.readFile("src/test/resources/grades.txt");
-		//System.out.println(methodOutput);
+		//the expected results
 		int[] expectedArray = {3,5,7,9,1,0,2,4,6,8};
+		//check if the output is the same as the expected results
 		assertArrayEquals("checks if it produces the files output", expectedArray, methodOutput); 
 		
 	}
