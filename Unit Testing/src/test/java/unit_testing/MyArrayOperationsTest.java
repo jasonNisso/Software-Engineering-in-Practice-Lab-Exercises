ced25d2af66a1c6a9674fcd28ed0ce2b30b169d5
Iason Nissopoulos <IasonNissopoulos@users.noreply.github.com>
2018-04-17 17:37:58 +0300

    Add files via upload
    Use mock's when().thenReturn()

--- Unit Testing/src/test/java/unit_testing/MyArrayOperationsTest.java
+++ Unit Testing/src/test/java/unit_testing/MyArrayOperationsTest.java
@@ -37,7 +37,7 @@ public class MyArrayOperationsTest {
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 	@Test
-	public void test_gradeFrequencies_RuleException()  {
+	public void test_gradeFrequencies_RuleException() throws FileNotFoundException  {
 		thrown.expect(FileNotFoundException.class);
 		mao.gradeFrequencies("src/test/resources/grades.txt", mfu);
 	}
@@ -46,10 +46,10 @@ public class MyArrayOperationsTest {
 	 * with normal input values and mocking. 
 	 */
 	@Test
-	public void test_gradeFrequencies_Mocking() {
+	public void test_gradeFrequencies_Mocking() throws FileNotFoundException {
 		// Pre-define the results of the mfu calls
 		//when(mfu.readFile(filepath)).thenReturn(new int[]{0,2,9,4,1,6,7,7,10,3,8,1,1});
-		//when(mao.gradeFre(filepath)).thenReturn(new int[]{0,2,9,4,1,6,7,7,10,3,8,1,1});
+		when(mfu.readFile(filepath)).thenReturn(new int[]{3,5,7,9,1,0,2,4,6,8});
 
 		//expected MyArrayOperations.gradeFrequencies() result
 		//int [] expected = new int[]{1,3,1,1,1,0,1,2,1,1,1};
