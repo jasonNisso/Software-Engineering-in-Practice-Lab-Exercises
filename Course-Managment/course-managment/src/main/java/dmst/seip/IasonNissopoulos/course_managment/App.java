05b4912cfbcef2041861fec4ef0e1241d53ae23e
Iason Nissopoulos <IasonNissopoulos@users.noreply.github.com>
2018-05-04 00:24:58 +0300

    Initialize course-managment project

---
+++ Course-Managment/course-managment/src/main/java/dmst/seip/IasonNissopoulos/course_managment/App.java
@@ -0,0 +1,13 @@
+package dmst.seip.IasonNissopoulos.course_managment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
